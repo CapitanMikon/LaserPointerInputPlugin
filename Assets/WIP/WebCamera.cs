76005361f7fbf55b9997e8cbdcdc74533addc421
Mikon <77883550+CapitanMikon@users.noreply.github.com>
2023-02-13 13:50:46 +0100

    TMP package, first version

--- Assets/WIP/WebCamera.cs
+++ Assets/WIP/WebCamera.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,12 +10,26 @@ public class WebCamera : MonoBehaviour
     [SerializeField] private RawImage outputImageHolder;
     
     private WebCamTexture webCamTexture;
-    private Color[] webCamPixels;
+    private Color32[] webCamPixels;
     
     private Texture2D resultImage;
 
     private IEnumerator laserDetectorCoroutine;
 
+    private const double R_VALUE = 0.299;
+    private const double G_VALUE = 0.587;
+    private const double B_VALUE = 0.114;
+    private const double PIXEL_LUMINANCE_TRESHOLD = 100;
+
+    private Border top;
+    private Border bottom;
+    private Border left;
+    private Border right;
+
+    [SerializeField] private GameObject markerSprite;
+    private RectTransform markerTransform;
+    //[SerializeField] private TextMeshProUGUI text;
+    
     void Start()
     {
         WebCamDevice[] devices = WebCamTexture.devices;
@@ -33,7 +49,16 @@ public class WebCamera : MonoBehaviour
         resultImage = new Texture2D(webCamTexture.width, webCamTexture.height);
         LogWebcamInfo();
         
-        laserDetectorCoroutine = LaserDetectorProcess();
+        //setUp
+        top = new Border(-1, 1920, 0, 0);
+        bottom = new Border(-1, 0, 0, 0);
+        left = new Border(0, -1, 0, 0);
+        right = new Border(1080, -1, 0, 0);
+        
+        markerTransform = markerSprite.GetComponent<RectTransform>();
+        
+        //laserDetectorCoroutine = GreyedImageProcess();
+        laserDetectorCoroutine = LaserPointerPositionUpdaterProcess();
         StartCoroutine(laserDetectorCoroutine);
     }
 
@@ -54,12 +79,12 @@ public class WebCamera : MonoBehaviour
         
     }
 
-    IEnumerator LaserDetectorProcess()
+    /*IEnumerator GreyedImageProcess()
     {
         for (; ; )
         {
             yield return new WaitForEndOfFrame();
-            webCamPixels = webCamTexture.GetPixels();
+            webCamPixels = webCamTexture.GetPixels32();
 
             for (int i = 0; i < webCamPixels.Length; i++)
             {
@@ -69,5 +94,156 @@ public class WebCamera : MonoBehaviour
             resultImage.Apply();
             outputImageHolder.texture = resultImage;
         }
+    }*/
+    
+    IEnumerator LaserPointerPositionUpdaterProcess()
+    {
+        for (; ; )
+        {
+            yield return new WaitForEndOfFrame();
+            webCamPixels = webCamTexture.GetPixels32();
+
+            for (int i = 0; i < webCamPixels.Length; i++)
+            {
+                var pixelLuminance = CalculateLuminance(webCamPixels[i]);
+                int currentX = i % webCamTexture.requestedWidth;
+                int currentY = i / webCamTexture.requestedWidth;
+
+                if (pixelLuminance > PIXEL_LUMINANCE_TRESHOLD)
+                {
+                    //UpdateBorders(currentX, currentY, pixelLuminance);
+                    markerSprite.transform.position = new Vector3(currentX, currentY, 0);
+                }
+
+            }
+
+            //UpdatePointerImage();
+            //ResetBorders();
+        }
+    }
+
+    private void UpdateBorders(int x, int y, double luminance)
+    {
+        string t = "";
+        //top
+        if (top.GetLuminance() < luminance && y < top.GetPosY())
+        {
+            top.SetPos(0,y);
+            top.SetLuminance(luminance);
+            t += "TOP: " + y + "; lum: \n" + luminance;
+        }
+        
+        //bottom
+        if (bottom.GetLuminance() < luminance && y > bottom.GetPosY())
+        {
+            bottom.SetPos(0, y);
+            bottom.SetLuminance(luminance);
+            t += "BOTTOM: " + y + "; lum: \n" + luminance;
+        }
+        
+        //left
+        if (left.GetLuminance() < luminance && x < left.GetPosX())
+        {
+            left.SetPos(x, 0);
+            left.SetLuminance(luminance);
+            t += "LEFT: " + x + "; lum: \n" + luminance;
+        }
+        
+        //right
+        if (right.GetLuminance() < luminance && x > right.GetPosX())
+        {
+            right.SetPos(x, 0);
+            right.SetLuminance(luminance);
+            t += "RIGHT: " + x + "; lum: \n" + luminance;
+        }
+
+        if (!t.Equals(""))
+        {
+            Debug.Log(t);
+        }
+        //text.text = t;
+    }
+
+    private static double CalculateLuminance(Color32 color32)
+    {
+        return R_VALUE * color32.r+ G_VALUE * color32.g + B_VALUE * color32.b;
+    }
+
+    private void UpdatePointerImage()
+    {
+        var centerX = right.GetPosX() - left.GetPosX();
+        var centerY = top.GetPosY() - bottom.GetPosY();
+        markerSprite.transform.localPosition = new Vector3(centerX, centerY, 0);
+    }
+
+    void ResetBorders()
+    {
+        top.SetValues(0, 1920, -1);
+        bottom.SetValues(0, -1, -1);
+        left.SetValues(-1, 0, -1);
+        right.SetValues(1080, 0, -1);
+    }
+}
+
+public class Border
+{
+    private double luminance;
+    
+    private int posX;
+    private int posY;
+
+    private int posIn2dArray;
+
+    public Border(int x, int y, float luminance)
+    {
+        posX = x;
+        posY = y;
+        this.luminance = luminance;
+    }
+    
+    public Border(int x, int y, float luminance, int posIn2dArray)
+    {
+        posX = x;
+        posY = y;
+        this.luminance = luminance;
+        this.posIn2dArray = posIn2dArray;
+    }
+
+    public double GetLuminance()
+    {
+        return luminance;
+    }
+
+    public int GetPosX()
+    {
+        return posX;
+    }
+    public int GetPosY()
+    {
+        return posY;
+    }
+
+    public void SetLuminance(double luminance)
+    {
+        this.luminance = luminance;
+    }
+
+    
+    public void SetPos(int x, int y)
+    {
+        posX = x;
+        posY = y;
+    }
+    
+    public void SetValues(int x, int y, double luminance)
+    {
+        posX = x;
+        posY = y;
+        this.luminance = luminance;
+    }
+
+    public void SetPosIn2dArray(int posIn2dArray)
+    {
+        this.posIn2dArray = posIn2dArray;
     }
 }
