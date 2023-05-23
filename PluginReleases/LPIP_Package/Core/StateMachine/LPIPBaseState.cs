df216a5fbfc3f2e84665493871c059a8b3f08610
Mikon <77883550+CapitanMikon@users.noreply.github.com>
2023-05-23 17:25:33 +0200

    release file organization

--- PluginReleases/LPIP_Package/Core/StateMachine/LPIPBaseState.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public abstract class LPIPBaseState
-{
-    public abstract void EnterState(LPIPCoreManager lpipCoreManager);
-    public abstract void UpdateState();
-    public abstract void ExitState();
-}
